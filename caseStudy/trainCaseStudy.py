cfe35640378bdf0428e04e5295311fe8444f18b1
csmagnano <chrismagnano@gmail.com>
2022-12-09 17:53:09 -0500

    Removed black magic lines

--- caseStudy/trainCaseStudy.py
+++ caseStudy/trainCaseStudy.py
@@ -77,8 +77,6 @@ def train(loader, model, optimizer, criterion,device):
     model.train()
     optimizer.zero_grad()  # Clear gradients.
     for data in loader:  # Iterate in batches over the training dataset.
-         if isinstance(data,list): #Black magic
-            data = data[0]
          data.to(device)
          out,e = model(data.x, data.edge_index, data.batch)  # Perform a single forward pass.
          loss = criterion(out[data.is_pred], data.y[data.is_pred])  # Compute the loss only where we have data
